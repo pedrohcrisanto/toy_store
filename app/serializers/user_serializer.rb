623cb4f6c8cc7ce8d730e3345cd6681f2f60855d
Pedro Crisanto <pedrohcrisanto@gmail.com>
2025-06-02 18:20:27 -0300

    serializers

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,6 @@
+# app/serializers/user_serializer.rb
+
+class UserSerializer
+  include JSONAPI::Serializer
+  attributes :id, :email, :created_at, :updated_at # Adicione os atributos que deseja expor
+end
