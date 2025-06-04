aedecfdb26f89f218015744e7392c9cbaef4bab8
Pedro Crisanto <pedrohcrisanto@gmail.com>
2025-06-03 21:07:21 -0300

    change controller

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,10 +1,10 @@
 # app/controllers/api/v1/clients_controller.rb
-class Api::V1::ClientsController < Api::V1::BaseController
+class ::Api::V1::ClientsController < ::Api::V1::BaseController
   # before_action :authenticate_user!
 
   # GET /api/v1/clients
   def index
-    result = Clients::List.call(q: params[:q])
+    result = ::Clients::List.call(q: params[:q])
     @pagy, @clients = pagy(result.data[:clients])
 
     if result.success?
@@ -12,56 +12,61 @@ class Api::V1::ClientsController < Api::V1::BaseController
                      message: result.data[:message],
                      meta: @pagy }, status: :ok
     else
-      render json: { message: result.data[:message], error: result.data[:error] }
+      render json: { message: result.data[:message],
+                     error: result.data[:error] }, status: :unprocessable_entity
     end
   end
 
   # GET /api/v1/clients/:id
   def show
-    result = Clients::Show.call(id: params[:id])
+    result = ::Clients::Show.call(id: params[:id])
 
     if result.success?
       render json: { data: blueprint(result.data[:client]),
                      message: result.data[:message]}, status: :ok
     else
-      render json: { message: result.data[:message], error: result.data[:error] }, status: :not_found
+      render json: { message: result.data[:message],
+                     error: result.data[:error] }, status: :not_found
     end
   end
 
   # POST /api/v1/clients
   def create
-    result = Clients::Create.call(params: client_params)
+    result = ::Clients::Create.call(params: client_params)
 
     if result.success?
       render json: { data: blueprint(result.data[:client]),
                      message: result.data[:message]}, status: :created
     else
-      render json: { message: result.data[:message], error: result.data[:error] }
+      render json: { message: result.data[:message],
+                     error: result.data[:error] }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/clients/:id
   def update
-    result = Clients::Update.call(params: client_params,
+    result = ::Clients::Update.call(params: client_params,
                                   id: params[:id])
 
     if result.success?
       render json: { data: blueprint(result.data[:client]),
                      message: result.data[:message]}, status: :ok
     else
-      render json: { message: result.data[:message], error: result.data[:error] }
+      render json: { message: result.data[:message],
+                     error: result.data[:error] }, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/clients/:id
   def destroy
-    result = Clients::Destroy.call(id: params[:id])
+    result = ::Clients::Destroy.call(id: params[:id])
 
     if result.success?
       render json: { data: blueprint(result.data[:client]),
                      message: result.data[:message]}, status: :ok
     else
-      render json: { message: result.data[:message], error: result.data[:error] }, status: :not_found
+      render json: { message: result.data[:message],
+                     error: result.data[:error] }, status: :not_found
     end
   end
 
@@ -72,6 +77,6 @@ class Api::V1::ClientsController < Api::V1::BaseController
   end
 
   def blueprint(collection)
-    ClientsBlueprint.render_as_json(collection)
+    ::ClientsBlueprint.render_as_json(collection)
   end
 end
